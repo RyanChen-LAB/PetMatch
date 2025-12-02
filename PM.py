2c6c5837094b7cc3183fa1f1fe98174bffe9e0fb
RyanChen-LAB <ryanchen220@gmail.com>
2025-12-02 11:08:53 +0800

    PM.py

--- PM.py
+++ PM.py
@@ -57,7 +57,7 @@ st.markdown("""
         border: none;
         border-radius: 15px;
         padding: 20px 10px; /* 加大高度 */
-        font-size: 1.3rem !important; /* 加大字體 */
+        font-size: 2.0 rem !important; /* 加大字體 */
         font-weight: 900 !important;
         width: 100%;
         text-shadow: 0px 1px 2px rgba(0,0,0,0.3);
